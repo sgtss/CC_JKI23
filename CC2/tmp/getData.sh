5254f17bbcc216ff285d5d0a2ac89b2b863e4fa6
Shrikant Sharma <sgt.shrikant.sharma@gmail.com>
2023-03-06 12:33:07 +0100

    CC2

--- CC2/tmp/getData.sh
+++ CC2/tmp/getData.sh
@@ -3,4 +3,4 @@
 #set -xe
 set -e
 
-wget -P sample_data/sample/ ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Nostoc_flagelliforme/latest_assembly_versions/GCF_002813575.1_ASM281357v1/* 
+wget -P sample_data/ref/ ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Nostoc_flagelliforme/latest_assembly_versions/GCF_002813575.1_ASM281357v1/* 
